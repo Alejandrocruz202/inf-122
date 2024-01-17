2aa2c4082fd7a4640dda62790ebab3ca916d8330
Alejandrocruz202 <119201856+Alejandrocruz202@users.noreply.github.com>
2024-01-17 15:38:21 +0000

    listo

---
+++ keepfalso/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Nota from './componentes/Nota';
+import Listadenotas from "./componentes/Listadenotas"
+function App() {
+  return (
+    
+    <Listadenotas/>
+    
+  );
+}
+
+export default App;
