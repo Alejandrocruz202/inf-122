08eed3100087e75aa93744267af71d968baa838e
Alejandrocruz202 <119201856+Alejandrocruz202@users.noreply.github.com>
2024-01-19 15:56:57 +0000

    dafm g,sgjksdfhg

--- ultimas/src/componentes/Navbar.js
+++ ultimas/src/componentes/Navbar.js
@@ -1,23 +1,28 @@
 import React from "react";
-import {Link} from "react";
-function Navbar(){
-    return(
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Inicio</Link>
-                </li>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-                <li>
-                    <Link to="/registro">Refitro</Link>
-                </li>
-                <li>
-                    <Link to="/perfil">Perfil</Link>
-                </li>
-            </ul>
-        </nav>
+import { Link } from "react";
+import { Outlet } from "react-router-dom";
+function Navbar() {
+    return (
+        <>
+            <nav>
+
+                <ul>
+                    <li>
+                        <Link to="/">Inicio</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                    <li>
+                        <Link to="/registro">Refitro</Link>
+                    </li>
+                    <li>
+                        <Link to="/perfil">Perfil</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Outlet />
+        </>
     );
 }
 export default Navbar;
