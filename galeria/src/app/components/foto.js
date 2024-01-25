e5664a8b64cacb3e2ea4a2b1199f2267bf7035a8
Alejandrocruz202 <119201856+Alejandrocruz202@users.noreply.github.com>
2024-01-25 15:34:10 +0000

    puto el que copie

--- galeria/src/app/components/foto.js
+++ galeria/src/app/components/foto.js
@@ -6,15 +6,19 @@ import style from "./foto.module.css";
 function Foto(){
     const [perrito,setPerrito]=useState("/vercel.svg");
     const [estado , setEstado]=useState("esperando");
+    const [habilidades,setHabilidades]=useState("esperando")
     const url = "https://pokeapi.co/api/v2/pokemon/meowth";
     useEffect(()=>{
         fetch(url)
         .then(res=>res.json())
-        .then(data=>{setPerrito(data.message),setEstado(data.status)})
+        .then(data=>{setPerrito(data.sprites.front_default),
+            setEstado(data.species.name),
+            setHabilidades(data.abilities[0].ability.name)})
     },[]);
     return(
         <div className={style.contenedor}>
             <h1>{estado}</h1>
+            <h2>{habilidades}</h2>
             <Image src={perrito} height={300} width={300}/>
         </div>
     );
